3c2debf38620e388ebc5b5325a77cc188d3f5b43
Kyumin Han <4rbals30@naver.com>
2022-03-02 15:29:10 +0900

    axios 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,25 @@
 import type { NextPage } from 'next'
+
 import { FormEvent, useCallback, useState } from 'react'
 import { TodoItem, useStore } from './store'
+import { useForm } from 'react-hook-form'
+import produce from 'immer'
+import axios from 'axios'
+import useSWR from 'swr'
+
+const client = axios.create({
+  baseURL: 'http://localhost:4444',
+  withCredentials: true,
+})
+
+// client
+//   .get('/todos')
+//   .then(res => {
+//     console.log(res.data.payload)
+//   })
+//   .catch(error => {
+//     console.log(error)
+//   })
 
 const Home: NextPage = () => {
   return (
@@ -15,6 +34,7 @@ const Home: NextPage = () => {
 
 function TodoList() {
   const [newText, setNewText] = useState('')
+  const { handleSubmit, register } = useForm()
   const {
     hideCompleted,
     toggleHideCompleted,
@@ -25,14 +45,31 @@ function TodoList() {
 
   function addTodoItem(e: FormEvent<HTMLFormElement>) {
     e.preventDefault()
-    addItem({ id: Date.now(), title: newText, done: false })
+    addItem({ id: Date.now(), content: newText, done: false })
     setNewText('')
   }
+
+  // const addTodoItem1 = (e: FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault()
+  //   addItem({ id: Date.now(), title: newText, done: false })
+  //   setNewText('')
+  // }
+
+  const onSubmit = useCallback(
+    (data: any) => {
+      // addItem({ id: Date.now(), content: newText, done: false })
+      // setNewText('')
+      client
+        .post('/todos', { content: newText })
+        .then(res => console.log(res.data.payload))
+    },
+    [newText]
+  )
 
   const addTodoItem2 = useCallback(
     (e: FormEvent<HTMLFormElement>) => {
       e.preventDefault()
-      addItem({ id: Date.now(), title: newText, done: false })
+      addItem({ id: Date.now(), content: newText, done: false })
       setNewText('')
     },
     [newText]
@@ -40,7 +77,7 @@ function TodoList() {
 
   return (
     <>
-      <form onSubmit={addTodoItem} className="mt-5 flex gap-4">
+      <form onSubmit={handleSubmit(onSubmit)} className="mt-5 flex gap-4">
         <label className="input-group input-group-sm">
           <span>할 일</span>
           <input
@@ -48,6 +85,7 @@ function TodoList() {
             placeholder="할 일을 입력하세요."
             className="input input-bordered input-sm"
             value={newText}
+            {...register('newText')}
             onChange={e => setNewText(e.currentTarget.value)}
           />
         </label>
@@ -87,31 +125,81 @@ function TodoList() {
 }
 
 function List() {
-  const { hideCompleted, items } = useStore()
+  const { hideCompleted } = useStore()
+  const fetcher = (url: string) => client.get(url).then(res => res.data.payload)
+  const { data } = useSWR<TodoItem[]>('/todos', fetcher)
   return (
     <ul>
-      {items
-        .filter(li => !li.done || !hideCompleted)
-        .map(li => (
-          <ListItem item={li} key={li.id}></ListItem>
-        ))}
+      {data && data.map(li => <ListItem item={li} key={li.id}></ListItem>)}
     </ul>
   )
 }
+// .filter(li => !li.done || !hideCompleted)
 
 function ListItem({ item }: { item: TodoItem }) {
-  const { toggleItem, removeItem } = useStore()
+  const { toggleItem, updateItem, removeItem } = useStore()
+  const [updateText, setUpdateText] = useState(item.content)
+  const [editing, setEditing] = useState(false)
+
+  const onClickEditButton = () => {
+    setEditing(true)
+  }
+
+  const onUpdate = (e: FormEvent<HTMLInputElement>) => {
+    setUpdateText(e.currentTarget.value)
+  }
+
+  const onEditSubmit = (id: number) => {
+    updateItem(id, updateText)
+    client.put(`/todos/${id}`, { content: updateText, done: false })
+    // .then(res => console.log(res.data.payload))
+    setEditing(false)
+  }
+
   return (
-    <li className={item.done ? 'done' : ''}>
+    <li>
       <input
         type="checkbox"
-        name=""
-        id=""
         checked={item.done}
-        onChange={() => toggleItem(item)}
-      />{' '}
-      {item.title}{' '}
-      <button onClick={() => removeItem(item)} className="delete">
+        onChange={() =>
+          client
+            .put(`/todos/${item.id}`, {
+              content: item.content,
+              done: !item.done,
+            })
+            .then(res => console.log(res.data.payload))
+        }
+      />
+      {editing ? (
+        <input
+          type="text"
+          className="input input-bordered input-sm"
+          value={updateText}
+          onChange={onUpdate}
+        />
+      ) : (
+        `${item.content}`
+      )}
+
+      {!item.done ? (
+        editing ? (
+          <button type="submit" onClick={() => onEditSubmit(item.id)}>
+            확인
+          </button>
+        ) : (
+          <button type="button" onClick={onClickEditButton}>
+            수정
+          </button>
+        )
+      ) : null}
+      <button
+        onClick={async () =>
+          await client
+            .delete(`/todos/${item.id}`)
+            .then(res => console.log(res.data.payload))
+        }
+        className="delete"
+      >
         delete
       </button>
     </li>
--- pages/store.tsx
+++ pages/store.tsx
@@ -1,9 +1,10 @@
 import create from 'zustand'
 import produce from 'immer'
+import axios from 'axios'
 
 export interface TodoItem {
   id: number
-  title: string
+  content: string
   done: boolean
 }
 
@@ -11,6 +12,7 @@ export interface State {
   items: TodoItem[]
   hideCompleted: boolean
   addItem(item: TodoItem): void
+  updateItem(id: number, content: string): void
   toggleHideCompleted(): void
   toggleItem(item: TodoItem): void
   removeItem(item: TodoItem): void
@@ -27,19 +29,57 @@ export const useStore = create<State>(set => ({
         draft.items.push(item)
       })
     ),
+  updateItem: (id: number, content: string) =>
+    set(
+      produce((draft: State) => {
+        draft.items = draft.items.map(
+          produce((todoItem: TodoItem) => {
+            if (todoItem.id === id) {
+              todoItem.content = content
+            }
+          })
+        )
+      })
+    ),
   removeItem: (item: TodoItem) =>
-    set(state => ({
-      items: state.items.filter(it => it.id !== item.id),
-    })),
+    // set(state => ({
+    //   items: state.items.filter(it => it.id !== item.id),
+    // })),
+    set(
+      produce((draft: State) => {
+        draft.items = draft.items.filter(it => it.id !== item.id)
+      })
+    ),
   toggleItem: (item: TodoItem) =>
-    set(state => ({
-      items: state.items.map(it => ({
-        ...it,
-        done: it.id === item.id ? !item.done : it.done,
-      })),
-    })),
+    // set(state => ({
+    //   items: state.items.map(it => ({
+    //     ...it,
+    //     done: it.id === item.id ? !item.done : it.done,
+    //   })),
+    // })),
+    set(
+      produce((draft: State) => {
+        draft.items = draft.items.map(
+          produce((todoItem: TodoItem) => {
+            if (todoItem.id === item.id) {
+              todoItem.done = !todoItem.done
+            }
+          })
+        )
+      })
+    ),
   toggleHideCompleted: () =>
-    set(state => ({ hideCompleted: !state.hideCompleted })),
+    set(
+      // state => ({ hideCompleted: !state.hideCompleted })
+      produce((draft: State) => {
+        draft.hideCompleted = !draft.hideCompleted
+      })
+    ),
   removeCompleted: () =>
-    set(state => ({ items: state.items.filter(it => !it.done) })),
+    set(
+      produce((draft: State) => {
+        draft.items = draft.items.filter(it => !it.done)
+      })
+    ),
+  // state => ({ items: state.items.filter(it => !it.done) })
 }))
